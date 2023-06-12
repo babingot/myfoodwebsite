1c5641db30d959f907439a26a6c72fba036981b8
babingot <136396505+babingot@users.noreply.github.com>
2023-06-13 07:57:33 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function validation(){
+    if(document.Formfill.Username.value==""){
+        document.getElementById("result").innerHTML="Enter Username*";
+        return false;
+    }
+    else if(document.Formfill.Username.value.length<6){
+        document.getElementById("result").innerHTML="Atleast six letter*";
+        return false;
+    }
+    else if(document.Formfill.Email.value==""){
+    document.getElementById("result").innerHTML="Enter your Email*";
+    return false;
+    }
+    else if(document.Formfill.Password.value==""){
+    document.getElementById("result").innerHTML="Enter your Password*";
+    return false;
+    }
+    else if(document.Formfill.Password.value.length<6){
+        document.getElementById("result").innerHTML="Password must be 6-digits*";
+        return false;
+    }
+    else if(document.Formfill.CPassword.value==""){
+        document.getElementById("result").innerHTML="Enter Confirm Password*";
+        return false;
+    }
+    else if(document.Formfill.CPassword.value !== document.Formfill.Password.value){
+        document.getElementById("result").innerHTML="Password does'nt matched*";
+        return false;
+    }
+    else if(document.Formfill.Password.value == document.Formfill.CPassword.value){
+       popup.classList.add("open-slide")
+        return false;
+    }
+
+    
+}
+var popup=document.getElementById('popup');
+function CloseSlide(){
+    popup.classList.remove("open-slide")
+}
+
+
+
+
+let menu = document.querySelector("menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", function(){
+    navbar.classList.toggle("active");
+});
+
+window.onscroll = () =>  {
+navbar.classList.remove("active");
+};
+
+//FILE URL
+js=document.getElementById('URL_1').setAttribute("href","https://www.facebook.com/abulenciatrixie");
+js=document.getElementById('URL_2').setAttribute("href","https://twitter.com/_badgaltrix");
+js=document.getElementById('URL_3').setAttribute("href","https://www.instagram.com/");
